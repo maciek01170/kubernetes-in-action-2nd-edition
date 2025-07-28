15d7f19081ce3456e1438d78abd47aebe2b4f31f
maciek <maciej.macowicz@epfl.ch>
2025-07-28 17:17:59 +0200

    test: sleep for 1h before going on

--- Chapter05/network-connectivity-checker-0.1/docker_entrypoint.sh
+++ Chapter05/network-connectivity-checker-0.1/docker_entrypoint.sh
@@ -3,13 +3,13 @@
 host=${1:-1.1.1.1}
 echo "Checking network connectivity to ${host} ..."
 
-ping ${host} -c 1 >/dev/null 2>/dev/null
+ping ${host} -c 1 # >/dev/null 2>/dev/null
 pingExitCode=$?
-
+sleep 1h
 if [ $pingExitCode = 0 ]; then
-  echo "Host appears to be reachable"
+  echo "Host $host appears to be reachable"
   exit 0
 else
-  echo "Host unreachable!"
+  echo "Host $host unreachable!"
   exit 1
 fi
